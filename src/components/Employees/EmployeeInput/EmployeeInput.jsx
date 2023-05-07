237a288ba8efd0fe083d24601dbf6ef39f6c52d4
titi7454 <konstantin19961996@abv.bg>
2023-05-07 05:19:07 +0200

    buttons changed

--- src/components/Employees/EmployeeInput/EmployeeInput.jsx
+++ src/components/Employees/EmployeeInput/EmployeeInput.jsx
@@ -66,7 +66,7 @@ const EmployeeInput = ({ addEmployee }) => {
         placeholder="Salary"
       />
       <button
-        className="lg:w-1/12 mx-auto mt-2 py-1 text-xl rounded-lg bg-gray-600"
+        className="w-1/3 lg:w-[13%] mx-auto mt-2 py-1 text-xl rounded-lg bg-gray-600"
         onClick={handleAddEmployee}
       >
         Submit
--- src/components/Tasks/TodoInput/TodoInput.jsx
+++ src/components/Tasks/TodoInput/TodoInput.jsx
@@ -61,7 +61,7 @@ const TodoInput = ({ addTodo, employeeNames }) => {
         placeholder="Due date"
       />
       <button
-        className="lg:w-1/12 mx-auto mt-2 py-1 text-xl rounded-lg bg-gray-600"
+        className="w-1/3 lg:w-[13%] mx-auto mt-2 py-1 text-xl rounded-lg bg-gray-600"
         onClick={handleAddTodo}
       >
         Submit
